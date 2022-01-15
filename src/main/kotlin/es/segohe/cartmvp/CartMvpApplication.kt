fd8726d44f2efe3dded4bd41fdee4f67e86fd1ba
Sergio <sergio.gomez.develop@gmail.com>
2022-01-15 16:22:36 +0100

    second commit

---
+++ src/main/kotlin/es/segohe/cartmvp/CartMvpApplication.kt
@@ -0,0 +1,11 @@
+package es.segohe.cartmvp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CartMvpApplication
+
+fun main(args: Array<String>) {
+    runApplication<CartMvpApplication>(*args)
+}
---
+++ src/test/kotlin/es/segohe/cartmvp/CartMvpApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.segohe.cartmvp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CartMvpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
